0d6db64fb186eb758b9fa82e0ddef81605a90a0a
Mikalai Sitsko <mikalai.sitsko@hlag.com>
2026-05-30 16:34:32 +0200

    update booking workflow to include llmProvider in BookingResponse and adjust proposal request prompt

--- ai-tool-calling/src/main/java/me/sitsko/ai/booking/BookingResponse.java
+++ ai-tool-calling/src/main/java/me/sitsko/ai/booking/BookingResponse.java
@@ -1,13 +1,14 @@
 package me.sitsko.ai.booking;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Builder;
 import me.sitsko.ai.schedule.VoyageData;
 
 @Builder
 public record BookingResponse(
-
 		String userRequest,
 		BookingStructuredRequest bookingStructuredRequest,
 		VoyageData voyageData,
+		String llmProvider,
 		String error) {
 }
--- ai-tool-calling/src/main/java/me/sitsko/ai/booking/BookingWorkflow.java
+++ ai-tool-calling/src/main/java/me/sitsko/ai/booking/BookingWorkflow.java
@@ -2,9 +2,11 @@ package me.sitsko.ai.booking;
 
 import dev.langchain4j.agentic.declarative.Output;
 import dev.langchain4j.agentic.declarative.SequenceAgent;
+import io.smallrye.config.SmallRyeConfig;
 import me.sitsko.ai.parser.ParserAgent;
 import me.sitsko.ai.schedule.ScheduleAgent;
 import me.sitsko.ai.schedule.VoyageData;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 public interface BookingWorkflow {
 
@@ -15,7 +17,10 @@ public interface BookingWorkflow {
 
 	@Output
 	static BookingResponse output(String userRequest, BookingStructuredRequest bookingStructuredRequest, VoyageData voyageData) {
-		return new BookingResponse(userRequest, bookingStructuredRequest, voyageData, "");
+		SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
+
+		String llmProvider = config.getValue("quarkus.langchain4j.chat-model.provider", String.class);
+		return new BookingResponse(userRequest, bookingStructuredRequest, voyageData, llmProvider, "");
 	}
 }
 
